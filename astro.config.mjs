0d2fc3c70cb1072097aa320d0d5edeceb7dacb16
Karthik Jasti <karthikjasti08@gmail.com>
2026-07-15 00:50:19 -0500

    Remove site and base config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://karthikjasti.github.io/portfolio',
-  base: '/portfolio',
   integrations: [mdx(), tailwind()]
 });
